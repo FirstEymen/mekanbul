16d8f36f1f69d9facc5bcffc5af87620120692f9
Eymen Bilir <a.eymenbilir@hotmail.com>
2022-11-11 16:46:42 +0300

    odev4

---
+++ app_server/models/db.js
@@ -0,0 +1,29 @@
+var mongoose=require("mongoose");
+var dbURI="mongodb://localhost/mekaunbl";
+//var dbURI="mongodb+srv://eymen:eymen993@mekanbul.kgdgtq5.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+    
+}
+process.on("SIGINT", function(){
+    kapat("Uygulama kapatıldı", function(){
+        process.exit(0);
+    });
+});
+mongoose.connection.on("connected", function(){
+    console.log(dbURI+"adresindeki veritabanına bağlandı.");
+});
+
+mongoose.connection.on("disconnected", function(){
+    console.log("Bağlantı koptu");
+});
+
+mongoose.connection.on("error", function(){
+    console.log("Bağlantı hatası");
+});
+
+require("./mekansema");
